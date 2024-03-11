7e4a71118c0b9888da6da0f34c9c7d606ad6bb75
Bijay Sau <bijaysaha594@gmail.com>
2024-03-11 08:42:50 +0530

    Personal Portfolio

---
+++ main.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".text",{
+    strings: [" Fullstack Webdeveloper"," Frontend Developer"," Backend Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backdelay: 1000,
+    loop: true
+});
+
+
+
